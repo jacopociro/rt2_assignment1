86cc635160234b9da0d26c0f89b856e3630dec12
jacopociro <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>
2022-08-25 10:57:49 +0000

    sphinx documentation

---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "cdfgmnprstu",
+  1: "g",
+  2: "ps",
+  3: "cdfgmnrtu",
+  4: "d"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "namespaces",
+  2: "files",
+  3: "functions",
+  4: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Namespaces",
+  2: "Files",
+  3: "Functions",
+  4: "Pages"
+};
+
